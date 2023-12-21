16f6694eb1186f79f53d0273b4bab62b706bf7bf
windy <kjyy2028@gmail.com>
2023-12-21 11:24:10 +0900

    기본완성

--- js/script.js
+++ js/script.js
@@ -7,5 +7,15 @@ $(function () {
 
     // 2. menu적용(인디케이터 커스텀)
     menu: '#myMenu',
+
+    // 3.필요 옵션 적용
+    // 스크롤링 기본값 700
+    scrollingSpeed: 1400,
+
+    // 고정시킬 요소
+    fixedElements: '#header',
+
+    // 영역 콘텐츠의 세로 정렬
+    verticalCentered: false,
   });
 });
