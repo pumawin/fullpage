46e6de2b1ebc08c792bc85e2fe92546e90b4310f
windy <kjyy2028@gmail.com>
2023-12-21 12:11:33 +0900

    메서드 활용

--- js/script.js
+++ js/script.js
@@ -18,4 +18,26 @@ $(function () {
     // 영역 콘텐츠의 세로 정렬
     verticalCentered: false,
   });
+
+  // go-top 버튼을 클릭했을 때
+  $('.btn-top').on('click', function (e) {
+    e.preventDefault();
+
+    // fullpage 메서드 :원하는 영역이름 탑버튼을 위해 사용
+    $.fn.fullpage.moveTo(1);
+  });
+
+  $('.btn-silent').on('click', function () {
+    $.fn.fullpage.silentMoveTo(1);
+  });
+
+  // btn-up 버튼을 클릭했을 때 한 영역 위로 이동
+  $('.btn-up').on('click', function () {
+    $.fn.fullpage.moveSectionUp();
+  });
+
+  // btn-down 버튼을 클릭했을 때 한 영역 아래로 이동
+  $('.btn-down').on('click', function () {
+    $.fn.fullpage.moveSectionDown();
+  });
 });
