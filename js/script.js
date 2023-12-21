f0832e8a04f8ad8c7ab74131266db4b9ffaf5dad
windy <kjyy2028@gmail.com>
2023-12-21 12:59:09 +0900

    이벤트 활용

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(function () {
+  const $btnTop = $('.btn-top');
+  const $header = $('#header');
+
   // fullpage 초기화
   $('#fullpage').fullpage({
     // 1. 앵커 설정
@@ -17,10 +20,40 @@ $(function () {
 
     // 영역 콘텐츠의 세로 정렬
     verticalCentered: false,
+
+    // 영역의 로딩이 되고 나서
+    afterLoad: function (anchorLink, index) {
+      var loadedSection = $(this); /* 위치확인 */
+
+      console.log('로딩된 후:' + anchorLink, index, loadedSection);
+      if (anchorLink === 'sec4') {
+        $btnTop.fadeIn();
+      }
+    },
+
+    // 영역에서 떠날때
+    onLeave: function (index, nextIndex, direction) {
+      var leavingSection = $(this);
+
+      console.log('영역에서 떠날 때' + index, nextIndex, direction);
+
+      if (index === 4 && direction === 'up') {
+        $btnTop.fadeOut();
+      }
+
+      if (direction === 'down') {
+        $header.addClass('hide');
+      } else {
+        $header.removeClass('hide');
+      }
+    },
   });
 
+  // 처음에는 숨기고
+  $btnTop.hide();
+
   // go-top 버튼을 클릭했을 때
-  $('.btn-top').on('click', function (e) {
+  $btnTop.on('click', function (e) {
     e.preventDefault();
 
     // fullpage 메서드 :원하는 영역이름 탑버튼을 위해 사용
